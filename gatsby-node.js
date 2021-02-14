aabf0f01dad83723f42d295b4077d4811f5f8e83
Ian de Jesus <ian@eskwelabs.com>
2021-02-14 10:08:33 +0900

    added createpages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,43 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+// NOTE Create pages allows us to programmatically create pages. For example, you have markdown files where each should be a page.
+// https://www.gatsbyjs.com/docs/creating-and-modifying-pages/#creating-pages-in-gatsby-nodejs
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const blogPost = path.resolve("./src/templates/blog-post.js")
+
+  const result = await graphql(
+    `
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              title
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const posts = result.data.allContentfulBlogPost.edges
+  posts.forEach(post => {
+    createPage({
+      path: `/blog/${post.node.slug}/`,
+      component: blogPost,
+      context: {
+        slug: post.node.slug,
+      },
+    })
+  })
+}
