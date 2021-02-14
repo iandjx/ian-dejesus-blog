c97cc73cada482af9eb0314e23a4ef121e368215
Ian de Jesus <ian@eskwelabs.com>
2021-02-14 10:29:59 +0900

    display blog post list in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,88 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import get from "lodash/get"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ArticlePreview from "../components/article-preview"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="text-gray-50">Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    <Link to="/blog/">Visit the Blog Page</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render() {
+    const siteTitle = get(this, "props.data.site.siteMetadata.title")
+    const posts = get(this, "props.data.allContentfulBlogPost.edges")
+    const [author] = get(this, "props.data.allContentfulPerson.edges")
+
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: "#fff" }}>
+          <Helmet title={siteTitle} />
+          {/* <img src="web-design.jpg" className="hero-background" /> */}
+
+          <div className="wrapper">
+            <h2 className="section-headline text-gray-400">Recent articles</h2>
+            <ul className="article-list">
+              {posts.map(({ node }) => {
+                return (
+                  <li key={node.slug}>
+                    <ArticlePreview article={node} />
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+    allContentfulPerson(
+      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
+    ) {
+      edges {
+        node {
+          name
+          shortBio {
+            shortBio
+          }
+          title
+          heroImage: image {
+            fluid(
+              maxWidth: 1180
+              maxHeight: 480
+              resizingBehavior: PAD
+              background: "rgb:000000"
+            ) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
