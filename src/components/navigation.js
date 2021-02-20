5fc36d029c53860bf29635dc1dbadbffe6b9d437
Ian de Jesus <ian@eskwelabs.com>
2021-02-20 11:48:31 +0900

    added initial styling

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import {
+  useTransition,
+  useChain,
+  animated,
+  config,
+  useSpring,
+} from "react-spring"
 
 const Navigation = () => {
-  const [navbarOpen, setNavbarOpen] = React.useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [offset, setOffset] = useState(0)
+  const props = useSpring({
+    from: {
+      backgroundColor: "rgba(255, 255, 255, 0)",
+    },
+    to: {
+      backgroundColor: offset > 50 ? "white" : "rgba(255, 255, 255, 0)",
+    },
+    config: {
+      duration: 1000,
+    },
+  })
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset)
+    }
+  }, [])
+
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-transparent mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <animated.nav
+        style={props}
+        className={`flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg   mb-3 fixed z-50 w-full`}
+      >
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between w-full">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 blackspace-no-wrap uppercase text-black"
+              className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 blackspace-no-wrap uppercase ${
+                offset > 50 ? "text-black" : "text-white"
+              } `}
               href="#pablo"
             >
               IAN DE JESUS
@@ -23,7 +54,7 @@ const Navigation = () => {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center" +
+              "lg:flex flex-grow items-center justify-self-end" +
               (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
@@ -34,7 +65,7 @@ const Navigation = () => {
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
                   href="#pablo"
                 >
-                  <i className="fab fa-linkedin-in text-lg leading-lg text-black opacity-75"></i>
+                  <i className="fab fa-linkedin text-lg leading-lg text-black opacity-75"></i>
                   <span className="ml-2">LinkedIn</span>
                 </a>
               </li>
@@ -48,10 +79,19 @@ const Navigation = () => {
                   <span className="ml-2">Github</span>
                 </a>
               </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fas fa-file text-lg leading-lg text-black opacity-75"></i>
+                  <span className="ml-2">Resume</span>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
-      </nav>
+      </animated.nav>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,71 @@
 import React from "react"
-import { Link } from "gatsby"
-import get from "lodash/get"
-import { Helmet } from "react-helmet"
+// import { Link } from "gatsby"
+// import get from "lodash/get"
+// import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-import ArticlePreview from "../components/article-preview"
-import Navigation from "../components/navigation"
-class IndexPage extends React.Component {
-  render() {
-    const siteTitle = get(this, "props.data.site.siteMetadata.title")
-    const posts = get(this, "props.data.allContentfulBlogPost.edges")
-    const [author] = get(this, "props.data.allContentfulPerson.edges")
+// import SEO from "../components/seo"
+// import ArticlePreview from "../components/article-preview"
+// import Hero from "../components/hero"
+// import Typical from "react-typical"
+// import BlogPost from "../components/blog/blog-post"
+// import "./test.css"
+import { useSpring, animated } from "react-spring"
 
-    return (
-      <Layout location={this.props.location}>
-        <div style={{ background: "#fff" }}>
-          <Helmet title={siteTitle} />
-          {/* <img src="web-design.jpg" className="hero-background" /> */}
+const IndexPage = () => {
+  //   // const siteTitle = get(this, "props.data.site.siteMetadata.title")
+  //   // const posts = get(this, "props.data.allContentfulBlogPost.edges")
+  //   // const [author] = get(this, "props.data.allContentfulPerson.edges")
 
-          <div className="wrapper">
-            <h2 className="section-headline text-gray-400">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+  const props = useSpring({
+    from: {
+      right: "-1000px",
+    },
+    to: {
+      right: "0px",
+    },
+    config: {
+      duration: 1000,
+    },
+  })
+
+  return (
+    // <div>hello</div>
+    <Layout>
+      <div style={{ background: "#fff" }}>
+        {/* <Helmet title={siteTitle} /> */}
+        <div className="bg flex justify-end items-center text-6xl px-24">
+          <animated.span style={props} className="relative">
+            Code Journal
+          </animated.span>
         </div>
-      </Layout>
-    )
-  }
+        <main className="bg-blue-50 mt-20 foo w-full p-10 mb-5">
+          <div className=" m-28 mt-36 p-4">
+            <div className="w-full border-solid border-2">
+              <article className="max-h-96 overflow-y-hidden flex">
+                <img className="w-3/5 px-3" src="image_1.jpg" alt="hello" />
+                <div className="p-3">
+                  <div className="text-sm text-gray-400 my-5">ILLUSTRATION</div>
+                  <div className="text-6xl font-bold">
+                    Build a website in minutes with Adobe
+                  </div>
+                </div>
+              </article>
+
+              <article className="max-h-96 overflow-y-hidden flex mb-5">
+                <img className="w-3/5 px-3" src="image_1.jpg" alt="hello" />
+                <div className="p-3">
+                  <div className="text-sm text-gray-400 my-5">ILLUSTRATION</div>
+                  <div className="text-6xl font-bold">
+                    Build a website in minutes with Adobe
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </main>
+      </div>
+    </Layout>
+  )
 }
 
 export default IndexPage
