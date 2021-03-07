f3b247f765ea751d856a3de8d9591142476bf49a
Ian de Jesus <ian@eskwelabs.com>
2021-03-07 21:20:10 +0900

    added links

--- src/components/blog/blog-list.js
+++ src/components/blog/blog-list.js
@@ -1,7 +1,7 @@
 import React from "react"
 import readingTime from "reading-time"
 import Img from "gatsby-image"
-
+import { Link } from "gatsby"
 const BlogList = ({ posts }) => {
   return (
     <>
@@ -13,34 +13,37 @@ const BlogList = ({ posts }) => {
           publishDate,
           description,
           body,
+          slug,
         } = post.node
         const readStats = readingTime(body.childMarkdownRemark.rawMarkdownBody)
         return (
-          <article className="max-h-96 overflow-y-hidden flex mb-5 w-full">
-            <div
-              className="px-3 overflow-hidden min-h-96"
-              style={{ minWidth: "60%" }}
-            >
-              <Img alt={title} fluid={heroImage.fluid} />
-            </div>
-            <div className="p-3">
-              <div className="text-sm text-gray-400 my-5">
-                {tags && tags.map(tag => <span className="mr-4">{tag}</span>)}
-              </div>
-              <div className="text-6xl font-bold mb-5 text-gray-800">
-                {title}
-              </div>
+          <Link to={`/blog/${slug}`}>
+            <article className="max-h-96 overflow-y-hidden flex mb-5 w-full">
               <div
-                className="text-gray-600"
-                dangerouslySetInnerHTML={{
-                  __html: description.childMarkdownRemark.html,
-                }}
-              />
-              <div className="text-sm mt-10 text-gray-500">
-                <span>{publishDate}</span> | <span> {readStats.text}</span>
+                className="px-3 overflow-hidden min-h-96"
+                style={{ minWidth: "60%" }}
+              >
+                <Img alt={title} fluid={heroImage.fluid} />
               </div>
-            </div>
-          </article>
+              <div className="p-3">
+                <div className="text-sm text-gray-400 my-5">
+                  {tags && tags.map(tag => <span className="mr-4">{tag}</span>)}
+                </div>
+                <div className="text-6xl font-bold mb-5 text-gray-800">
+                  {title}
+                </div>
+                <div
+                  className="text-gray-600"
+                  dangerouslySetInnerHTML={{
+                    __html: description.childMarkdownRemark.html,
+                  }}
+                />
+                <div className="text-sm mt-10 text-gray-500">
+                  <span>{publishDate}</span> | <span> {readStats.text}</span>
+                </div>
+              </div>
+            </article>
+          </Link>
         )
       })}
     </>
